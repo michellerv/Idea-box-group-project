9ac44ae13e5cfd412e761f8d1db3bf8c614ea2cd
Prissilla Escobar <prissilla988@gmail.com>
2023-06-02 10:58:28 -0600

    refactor: add nav and img to displayCard function.

--- main.js
+++ main.js
@@ -45,6 +45,8 @@ function displayCard() {
   for (var i = 0; i < savedIdeas.length; i++) {
     cardContainer.innerHTML +=
     `<article class="card" id= '${savedIdeas[i].id}'>
+    <nav class="card-nav"><img src="assets/delete.svg">
+    </nav>
     <p class="card-title">${savedIdeas[i].title}</p>
     <p class="card-body">${savedIdeas[i].body}</p>`
   }
